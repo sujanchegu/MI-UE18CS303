3eabe1a503d595c70598823a654c69a492a62942
Rishit Chaudhary <rishit.c@live.com>
2020-09-02 19:27:24 +0530

    Created my new test script for assignment 1

--- A1/DT_RC_TestCases.py
+++ A1/DT_RC_TestCases.py
@@ -12,7 +12,34 @@ def test_case():
     cough = 'no,yes,yes,no,yes,yes,no,no,yes,yes,yes,yes,yes,yes'.split(',')
     breathing_issues = 'no,yes,no,yes,yes,no,yes,yes,yes,no,no,yes,yes,no'\
         .split(',')
-    infected = 'no,yes,no,yes,yes,no,yes,yes,yes,yes,no,no,no,no'.split(',')
+    infected = 'no,yes,no,yes,yes,no,yes,yes,yes,yes,no,no,yes,no'.split(',')
+
+    fever = 'yes,yes,yes,yes,yes'.split(',')
+    cough = 'yes,no,yes,no,no'.split(',')
+    breathing_issues = 'yes,yes,yes,yes,yes'.split(',')
+    infected = 'yes,yes,yes,yes,yes'.split(',')
+
+
+    fever = 'no,no,no,no,no,yes,no,yes,no'.split(',')
+    cough = 'yes,yes,yes,yes,no,yes,yes,no,yes'.split(',')
+    breathing_issues = 'yes,yes,yes,no,no,yes,yes,yes,no'.split(',')
+    infected = 'yes,no,no,no,no,yes,no,no,yes'.split(',')
+
+    fever = 'no,no,no,no,no,yes,no,yes,no'.split(',')
+    cough = 'yes,yes,yes,yes,no,yes,yes,no,yes'.split(',')
+    breathing_issues = 'yes,yes,yes,no,no,yes,yes,yes,no'.split(',')
+    infected = 'yes,no,no,no,no,yes,no,no,no'.split(',')
+
+    fever = 'no,no,no,no,no,yes,no,yes,no,no,yes,no'.split(',')                                                                                             
+    cough = 'yes,yes,yes,yes,no,yes,yes,no,yes,yes,no,yes'.split(',')                                                                                       
+    breathing_issues = 'yes,yes,yes,no,no,yes,yes,yes,no,yes,yes,no'.split(',')                                                                                        
+    infected = 'yes,no,maybe,no,no,yes,maybe,no,maybe,maybe,no,maybe'.split(',')   
+
+    # fever = 'no,no,no'.split(',')
+    # cough = 'yes,yes,yes'.split(',')
+    # breathing_issues = 'yes,yes,yes'.split(',')
+    # infected = 'yes,no,no'.split(',')
+
 
     columns = [fever, cough, breathing_issues, infected]
 
@@ -42,43 +69,45 @@ def test_case():
         print("Test Case 1 for the function get_entropy_of_dataset FAILED")
 
     try:
-        if get_entropy_of_attribute(df, 'fever') >= 0.691 and \
-           get_entropy_of_attribute(df, 'fever') <= 0.695 :
+        if get_entropy_of_attribute(df, 'fever') >= 0.852 and \
+           get_entropy_of_attribute(df, 'fever') <= 0.862 :
             print("Test Case 2 for the function get_entropy_of_attribute PASSED")
         else:
-            print("Test Case 2 for the function get_entropy_of_attribute FAILED")
+            print("Test Case 2 for the function get_entropy_of_attribute FAILED", get_entropy_of_attribute(df, 'fever'))
 
     except:
          print("Test Case 2 for the function get_entropy_of_attribute FAILED")
 
     try:
-        if get_entropy_of_attribute(df, 'cough') >= 0.908 and \
-           get_entropy_of_attribute(df, 'cough') <= 0.914:
+        if get_entropy_of_attribute(df, 'cough') >= 0.945 and \
+           get_entropy_of_attribute(df, 'cough') <= 0.947:
             print("Test Case 3 for the function get_entropy_of_attribute PASSED")
         else:
-            print("Test Case 3 for the function get_entropy_of_attribute FAILED")
+            print("Test Case 3 for the function get_entropy_of_attribute FAILED", get_entropy_of_attribute(df, 'cough'))
 
     except:
         print("Test Case 3 for the function get_entropy_of_attribute FAILED")
 
-    try:
-        # columns=['fever', 'cough', 'breathing_issues', 'infected']
-        ans = get_selected_attribute(df)
-        dictionary = ans[0]
-        flag = (dictionary['fever'] >= 0.244 and dictionary['fever'] <= 0.248)\
-            and\
-               (dictionary['cough'] >= 0.0292 and dictionary['cough'] <= 0.0296)\
-            and\
-               (dictionary['breathing_issues'] >= 0.150 and dictionary['breathing_issues'] <= 0.154)\
-            and\
-               (ans[1] == 'fever')
+    # try:
+    #     # columns=['fever', 'cough', 'breathing_issues', 'infected']
+    ans = get_selected_attribute(df)
+    #     dictionary = ans[0]
+    #     flag = (dictionary['fever'] >= 0.244 and dictionary['fever'] <= 0.248)\
+    #         and\
+    #            (dictionary['cough'] >= 0.0292 and dictionary['cough'] <= 0.0296)\
+    #         and\
+    #            (dictionary['breathing_issues'] >= 0.150 and dictionary['breathing_issues'] <= 0.154)\
+    #         and\
+    #            (ans[1] == 'fever')
 
-        if flag:
-            print("Test Case 4 for the function get_selected_attribute PASSED")
-        else:
-            print("Test Case 4 for the function get_selected_attribute FAILED")
-    except:
-        print("Test Case 4 for the function get_selected_attribute FAILED")
+    print(ans)
+
+    #     if flag:
+    #         print("Test Case 4 for the function get_selected_attribute PASSED")
+    #     else:
+    #         print("Test Case 4 for the function get_selected_attribute FAILED")
+    # except:
+    #     print("Test Case 4 for the function get_selected_attribute FAILED")
 
 
 if __name__ == "__main__":
