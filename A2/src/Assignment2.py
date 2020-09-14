465e7875cebdcd80a99fb5b96b1850c14e61b5ca
Sujan CR <sujanchg@gmail.com>
2020-09-14 09:05:20 +0530

    Implement AStar

--- A2/src/Assignment2.py
+++ A2/src/Assignment2.py
@@ -20,14 +20,61 @@ Return : A list containing a list of all traversals [[],[],[]]
 NOTE : you are allowed to write other helper functions that you can call in the given fucntion
 '''
 
+import heapq
+
+
 def dfs(cost, start_point, goals):
     return []
 
 def ucs(cost, start_point, goals):
     return []
 
-def t3 = astar(cost, heuristic, start_point, goals):
-    return []
+def astar(cost, heuristic, start_point, goals):
+    frontier = []
+    infront = [0]*(len(cost))
+    infront[start_point] = 1
+    leastcost = [float('inf')]*len(cost)
+    leastparent = [-1]*len(cost)
+    #Format: (F, G, NameOfNode)
+    frontier.append((heuristic[start_point], 0, start_point))
+    leastcost[start_point] = 0
+    ptogoals = []
+    while(len(frontier) != 0):
+        temp = frontier.pop(0)
+        infront[temp[2]] = 0
+        for i in range(1,len(cost)):
+            if(cost[temp[2]][i] > 0):
+                cc = temp[1]+cost[temp[2]][i]+heuristic[i]
+                if(infront[i] == 0): #The node is not in frontier
+                    if(leastcost[i] > cc):
+                        leastcost[i] = cc
+                        leastparent[i] = temp[2]
+                        frontier.append((cc, cc-heuristic[i], i))
+                        infront[i] = 1
+                else: #The node is already in frontier
+                    for j,value in enumerate(frontier):
+                        if(value[2] == i):
+                            if((cc - heuristic[i]) < value[1]):
+                                frontier.pop(j)
+                                frontier.append((cc, cc-heuristic[i], i))
+                                leastcost[i] = cc
+                                leastparent[i] = temp[2]
+                if(i in goals):
+                    ptogoals.append((cc, cc, i, temp[2]))
+                    
+            heapq.heapify(frontier)
+    n1 = sorted(ptogoals)[0][2]
+    n2 = sorted(ptogoals)[0][3]
+    path = []
+    while(n1 != start_point):
+        path.append(n1)
+        n1 = n2
+        n2 = leastparent[n2]
+    path.append(start_point)
+    path.reverse()
+    return path    
+    
+
 
 def tri_traversal(cost, heuristic, start_point, goals):
     l = []
