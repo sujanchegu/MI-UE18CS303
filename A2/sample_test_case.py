99508a6df50987d7be6d9c3d63d1cba9cf76b9c7
Sujan CR <sujanchg@gmail.com>
2020-09-17 18:59:50 +0530

    Adding updated assignment files

--- A2/sample_test_case.py
+++ A2/sample_test_case.py
@@ -1,4 +1,4 @@
-from src.Assignment2 import *
+from src.MI_0316_1286_2057 import *
 
 
 def test_case():
@@ -28,14 +28,14 @@ def test_case():
         else:
             print("SAMPLE TEST CASE 1 FOR THE  DFS_TRAVERSAL FAILED")
     except:
-        print("TRY FAILED")
+        print("SAMPLE TEST CASE 1 FOR THE DFS_TRAVERSAL FAILED")
     try:
         if (tri_traversal(cost,heuristic, 1, [6, 7, 10]))[1] == [1, 5, 4, 7]:
             print("SAMPLE TEST CASE 2 FOR THE  UCS_TRAVERSAL PASSED")
         else:
             print("SAMPLE TEST CASE 2 FOR THE  UCS_TRAVERSAL FAILED")
     except:
-        print("TRY FAILED")
+        print("SAMPLE TEST CASE 2 FOR THE UCS_TRAVERSAL FAILED")
 
     try:
         if (tri_traversal(cost,heuristic, 1, [6, 7, 10]))[2] == [1, 5, 4, 7]:
@@ -43,7 +43,7 @@ def test_case():
         else:
             print("SAMPLE TEST CASE 3 FOR THE  A_star_TRAVERSAL FAILED")
     except:
-        print("TRY FAILED")
+        print("SAMPLE TEST CASE 3 FOR THE A_star_TRAVERSAL FAILED")
 
 
 test_case()
--- A2/src/MI_0316_1286_2057.py
+++ A2/src/MI_0316_1286_2057.py
@@ -1,58 +1,4 @@
-'''
-Function tri_traversal - performs DFS, UCS and A* traversals and returns the
-                         path for each of these traversals
-
-n - Number of nodes in the graph
-m - Number of goals ( Can be more than 1)
-1<=m<=n
-Cost - Cost matrix for the graph of size (n+1)x(n+1)
-IMP : The 0th row and 0th column is not considered as the starting index
-        is from 1 and not 0.
-Refer the sample test case to understand this better
-
-Heuristic - Heuristic list for the graph of size 'n+1'
-IMP : Ignore 0th index as nodes start from index value of 1
-Refer the sample test case to understand this better
-
-start_point - single start node
-goals - list of size 'm' containing 'm' goals to reach from start_point
-
-Return : A list containing a list of all traversals [[],[],[]]
-
-NOTE : you are allowed to write other helper functions that you can call
-        in the given fucntion
-'''
-
-import heapq
-from collections import deque
-
-
-# Defining the Goal Test Function
-def goalTest(state, goals):
-    return state in goals
-
-
-# Get a list of the neighbours of the popped node.
-# We return a list of the indices of the neighbours
-def getNeighbours(adjList):
-    neighbourList = []
-
-    # NOTE: We ignore the first node (as 1 based indexing for nodes has
-    # been used)
-    for index, node in enumerate(adjList[1::], start=1):
-        # If the value is not -1, then there is a path/edge from the
-        # popped node to the node
-        if node != -1:
-            neighbourList.append(index)
-
-    # NOTE: The list is reversed so that we can add the nodes into
-    # the stack in reverse lexicographical order, so that
-    # while popping from the stack we can retrieve them in
-    # lexicographical order
-    return neighbourList[::-1]
-
-
-def dfs(cost, start_point, goals):
+def A_star_Traversal(cost, heuristic, start_point, goals):
     # Frontier for DFS, i.e. the stack
     stack = deque()
 
@@ -97,8 +43,7 @@ def dfs(cost, start_point, goals):
     # we return an empty list
     return []
 
-
-def ucs(cost, start_point, goals):
+def UCS_Traversal(cost, start_point, goals):
     l = []
 
     # n = total number of nodes + 1
@@ -161,60 +106,111 @@ def ucs(cost, start_point, goals):
     return l
 
 
-def astar(cost, heuristic, start_point, goals):
-    frontier = []
-    infront = [0]*(len(cost))
-    infront[start_point] = 1
-    leastcost = [float('inf')]*len(cost)
-    leastparent = [-1]*len(cost)
-    # Format: (F, G, NameOfNode)
-    frontier.append((heuristic[start_point], 0, start_point))
-    leastcost[start_point] = 0
-    ptogoals = []
-    while(len(frontier) != 0):
-        temp = frontier.pop(0)
-        if(temp[2] in goals):
-            break
-        infront[temp[2]] = 0
-        for i in range(1, len(cost)):
-            if(cost[temp[2]][i] > 0):
-                cc = temp[1]+cost[temp[2]][i]+heuristic[i]
-                if(infront[i] == 0):  # The node is not in frontier
-                    if(leastcost[i] > cc):
-                        leastcost[i] = cc
-                        leastparent[i] = temp[2]
-                        frontier.append((cc, cc-heuristic[i], i))
-                        infront[i] = 1
-                else:  # The node is already in frontier
-                    for j, value in enumerate(frontier):
-                        if(value[2] == i):
-                            if((cc - heuristic[i]) < value[1]):
-                                frontier.pop(j)
-                                frontier.append((cc, cc-heuristic[i], i))
-                                leastcost[i] = cc
-                                leastparent[i] = temp[2]
-            heapq.heapify(frontier)
+# Defining the Goal Test Function
+def goalTest(state, goals):
+    return state in goals
 
-    n1 = temp[2]
-    n2 = leastparent[temp[2]]
-    path = []
-    while(n1 != start_point):
-        path.append(n1)
-        n1 = n2
-        n2 = leastparent[n2]
-    path.append(start_point)
-    path.reverse()
-    return path
+# Get a list of the neighbours of the popped node.
+# We return a list of the indices of the neighbours
+def getNeighbours(adjList):
+    neighbourList = []
+
+    # NOTE: We ignore the first node (as 1 based indexing for nodes has
+    # been used)
+    for index, node in enumerate(adjList[1::], start=1):
+        # If the value is not -1, then there is a path/edge from the
+        # popped node to the node
+        if node != -1:
+            neighbourList.append(index)
+
+    # NOTE: The list is reversed so that we can add the nodes into
+    # the stack in reverse lexicographical order, so that
+    # while popping from the stack we can retrieve them in
+    # lexicographical order
+    return neighbourList[::-1]
+
+
+def DFS_Traversal(cost, start_point, goals):
+    # Frontier for DFS, i.e. the stack
+    stack = deque()
+
+    # Set to hold the list of nodes explored
+    exploredSet = set()
+
+    # Path list to hold the path as it is being built
+    # As all paths start withe the start_point, it has
+    # been added in by default
+    path = [start_point]
+
+    # Push the inital node into the frontier/stack
+    stack.append(start_point)
+
+    # While the frontier/stack is not empty
+    while (stack):
+        # Pop a node from the frontier/stack
+        poppedNode = stack.pop()
+
+        # Check if the popped node is one of the goal states
+        if goalTest(poppedNode, goals) is True:
+            # Return the path found
+            return path
+
+        # If the popped node is not one of the goal states
+
+        # Add the node to the explored set
+        exploredSet.add(poppedNode)
+
+        # Expand the node, and get the list of neighbours' indices
+        poppedNodeNeighbours = getNeighbours(cost[poppedNode])
+
+        # Add the resulting nodes (child nodes) into the frontier,
+        # if they aren't already in the frontier or the explored set
+        for poppedNodeNeighbour in poppedNodeNeighbours:
+            if (poppedNodeNeighbour not in stack) and \
+               (poppedNodeNeighbour not in exploredSet):
+                stack.append(poppedNodeNeighbour)
+
+    # If we reached here, then that means that the frontier was emtpy
+    # before we reached a goal state, and hence there is no solution so
+    # we return an empty list
+    return []
+
+
+'''
+Function tri_traversal - performs DFS, UCS and A* traversals and returns the path for each of these traversals 
+
+n - Number of nodes in the graph
+m - Number of goals ( Can be more than 1)
+1<=m<=n
+Cost - Cost matrix for the graph of size (n+1)x(n+1)
+IMP : The 0th row and 0th column is not considered as the starting index is from 1 and not 0. 
+Refer the sample test case to understand this better
+
+Heuristic - Heuristic list for the graph of size 'n+1' 
+IMP : Ignore 0th index as nodes start from index value of 1
+Refer the sample test case to understand this better
+
+start_point - single start node
+goals - list of size 'm' containing 'm' goals to reach from start_point
+
+Return : A list containing a list of all traversals [[],[],[]]
+1<=m<=n
+cost[n][n] , heuristic[n][n], start_point, goals[m]
+
+NOTE : you are allowed to write other helper functions that you can call in the given fucntion
+'''
+
 
 
 def tri_traversal(cost, heuristic, start_point, goals):
     l = []
 
-    t1 = dfs(cost, start_point, goals)
-    t2 = ucs(cost, start_point, goals)
-    t3 = astar(cost, heuristic, start_point, goals)
+    t1 = DFS_Traversal(cost, start_point, goals)
+    t2 = UCS_Traversal(cost, start_point, goals)
+    t3 = A_star_Traversal(cost, heuristic, start_point, goals)
 
     l.append(t1)
     l.append(t2)
     l.append(t3)
     return l
+
