84f090683e14d6c46dbc2c3092eb6a519a696f03
Sanket <sanketdonty@gmail.com>
2020-11-11 20:49:47 +0530

    Changed the dropout thing

--- A3/new.py
+++ A3/new.py
@@ -4,13 +4,15 @@ import numpy as np
 
 class Layer:
     def __init__(self, _numInputs, _numNeurons):
-        self.prevShape = _numInputs
+        # Note, numInputs = number of neurons in the previous layer
+        self.prevShape = _numInputs + 1 # For bias
         self.shape = _numNeurons
         self.droprate = 0.1
         self.seed = np.random.RandomState(42)
-        self.biases = np.zeros((1, self.shape))
+        # self.biases = np.zeros((1, self.shape))
         sd = np.sqrt(6.0 / (self.prevShape + self.shape))
-        self.weights = np.random.uniform(-sd, sd, (self.prevShape, self.shape))
+        self.weights = np.random.uniform(-sd,
+                                         sd, (self.prevShape, self.shape))
 
     @classmethod
     def ReLU(cls, _nextInputs):
@@ -24,18 +26,27 @@ class Layer:
 
     def set_params(self, _weights, _biases):
         temp_weights = self.weights
-        temp_biases = self.biases
+        # temp_biases = self.biases
         self.weights = _weights
-        self.biases = _biases
-        return (temp_weights, temp_biases)
+        # self.biases = _biases
+        # return (temp_weights, temp_biases)
+        return temp_weights
 
     def get_params(self):
-        return (self.weights, self.biases)
+        # return (self.weights, self.biases)
+        return self.weights
+
+    def drop(self):
+        return np.random.binomial(1, self.droprate, size=self.shape) / (1 - self.droprate)
 
     def forward(self, _input, _train=False):
         self.output = np.dot(self.weights.T, _input) + self.biases
         if _train:
-            self.output *= np.random.binomial(1, self.droprate, size=self.shape) / (1 - self.droprate)
+            self.activeNeurons = drop()
+            self.output *= self.activeNeurons
+            
+            # self.output *= np.random.binomial(1, self.droprate, size=self.shape) / (1 - self.droprate)
+            # self.output /= (1 - self.droprate)
         #TODO return activationFunc(self.output)
 
     # def backward(self, _nextLayerInputs):
